8f400678c3392fe12855e89cab08a237e4306273
khoa24032001 <76723995+khoa24032001@users.noreply.github.com>
2022-06-17 21:28:29 +0700

    Update booking.php

--- booking.php
+++ booking.php
@@ -63,17 +63,18 @@
 
                     <div id="content">
                         <i class="fa fa-calendar" aria-hidden="true"></i>
-                        <input type="date" class=" booking_input2 booking_input_a booking_in" placeholder="Check in" name="Checkin" required="required">
+                        <input type="text" class=" booking_input2 booking_input_a booking_in" onfocus="(this.type='date')"
+                        onblur="(this.type='text')" placeholder="Check in" name="Checkin" required="required"> 
                     </div>
 
                     <div id="content">
-                        <i class="fa fa-users" aria-hidden="true"></i>
-                        <input type="number" id="input-group" placeholder="Single room" name="Singleroom"required="required">
+                        <i class="fa fa-bed" aria-hidden="true"></i>
+                        <input type="number" id="input-group" placeholder="Number of Single rooms" name="Singleroom"required="required">
                     </div>
 
                     <div id="content">
-                        <i class="fa fa-users" aria-hidden="true"></i>
-                        <input type="number" id="input-group" placeholder="Double room" name="Doubleroom"required="required">
+                        <i class="fa fa-bed" aria-hidden="true"></i>
+                        <input type="number" id="input-group" placeholder="Number of Double rooms" name="Doubleroom"required="required">
                     </div>
     
                     <!-- <div id="content">
@@ -88,15 +89,16 @@
                 <div id="second-group">
     
                     <div id="content">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <input type="text" id="input-group" placeholder="Your ID" name="ID" required="required">
+                        <i class="fa fa-address-card" aria-hidden="true"></i>
+                        <input type="number" id="input-group" placeholder="Your ID" name="ID" required="required">
                     </div>
     
                    
     
                     <div id="content">
                         <i class="fa fa-calendar" aria-hidden="true"></i>
-                        <input type="date" class=" booking_input2 booking_input_a booking_out" placeholder="Check out" name="Checkout"required="required">
+                        <input type="text" class=" booking_input2 booking_input_a booking_out" 
+                        onfocus="(this.type='date')" onblur="(this.type='text')"placeholder="Check out" name="Checkout"required="required">
                     </div>
     
                     <!-- <div id="content">
@@ -115,7 +117,7 @@
 
                     <div id="content">
                         <i class="fa fa-phone" aria-hidden="true"></i>
-                        <input type="text" id="input-group" placeholder="Phone number" name="Phone"required="required">
+                        <input type="number" id="input-group" placeholder="Phone number" name="Phone"required="required">
                     </div>
                 
                 </div>
